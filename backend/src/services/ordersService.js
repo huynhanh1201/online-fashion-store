f571ffa980522d9546cdbad2a532428bb04d3e91
huynhanh1201 <huynhhoanganh1201@gmail.com>
2025-05-18 18:29:53 +0700

    [#19] Test

--- backend/src/services/ordersService.js
+++ backend/src/services/ordersService.js
@@ -66,6 +66,8 @@ const createOrder = async (userId, reqBody, ipAddr) => {
       calculatedSubtotal += product.price * item.quantity
     }
 
+    console.log(calculatedSubtotal)
+
     // Xác thực mã giảm giá
     const validateCoupon = await couponsService.validateCoupon(userId, {
       couponCode,
@@ -78,6 +80,9 @@ const createOrder = async (userId, reqBody, ipAddr) => {
 
     const cartTotal = validateCoupon.newTotal || calculatedSubtotal
     const discountAmount = validateCoupon.discountAmount || 0
+
+    console.log('cartTotal', cartTotal)
+    console.log('total', total)
 
     // Kiểm tra tổng tiền từ FE
     if (cartTotal !== total) {
