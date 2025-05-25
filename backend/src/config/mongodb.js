e611a35f8c215d6926cd1f64061543337ad3ba8d
huynhanh1201 <huynhhoanganh1201@gmail.com>
2025-05-25 20:53:29 +0700

    [#1]

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -3,5 +3,7 @@ import mongoose from 'mongoose'
 import { env } from '~/config/environment'
 
 export async function CONNECT_DB() {
-  await mongoose.connect(env.MONGODB_URI + env.DATABASE_NAME)
+  await mongoose.connect(env.MONGODB_URI + env.DATABASE_NAME, {
+    autoIndex: process.env.BUILD_MODE !== 'prod'
+  })
 }
