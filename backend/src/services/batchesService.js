0861e7315d2f29cb2c7baf0eccac82711e0a9ed3
huynhanh1201 <huynhhoanganh1201@gmail.com>
2025-06-13 09:53:17 +0700

    [Feature] Filter Batches

--- backend/src/services/batchesService.js
+++ backend/src/services/batchesService.js
@@ -1,18 +1,91 @@
 import { BatchModel } from '~/models/BatchModel'
+import validatePagination from '~/utils/validatePagination'
+import getDateRange from '~/utils/getDateRange'
 
-const getBatchList = async () => {
-  const result = await BatchModel.find({ destroy: false })
-    .populate([
-      {
-        path: 'variantId',
-        select: 'name'
-      },
-      {
-        path: 'warehouseId',
-        select: 'name'
-      }
-    ])
-    .lean()
+const getBatchList = async (queryString) => {
+  let {
+    page = 1,
+    limit = 10,
+    search,
+    status,
+    sort,
+    filterTypeDate,
+    startDate,
+    endDate,
+    variantId,
+    warehouseId
+  } = queryString
+
+  // Kiểm tra dữ liệu đầu vào của limit và page
+  validatePagination(page, limit)
+
+  // Xử lý thông tin Filter
+  const filter = {}
+
+  if (variantId) filter.variantId = variantId
+
+  if (warehouseId) filter.warehouseId = warehouseId
+
+  if (status === 'true' || status === 'false') {
+    status = JSON.parse(status)
+
+    filter.destroy = status
+  }
+
+  if (search) {
+    filter.batchCode = { $regex: search, $options: 'i' }
+  }
+
+  const dateRange = getDateRange(filterTypeDate, startDate, endDate)
+
+  if (dateRange.startDate && dateRange.endDate) {
+    filter['createdAt'] = {
+      $gte: new Date(dateRange.startDate),
+      $lte: new Date(dateRange.endDate)
+    }
+  }
+
+  let sortField = {}
+
+  const sortMap = {
+    newest: { createdAt: -1 },
+    oldest: { createdAt: 1 }
+  }
+
+  if (sort) {
+    sortField = sortMap[sort]
+  }
+
+  const [batches, total] = await Promise.all([
+    BatchModel.find(filter)
+      .collation({ locale: 'vi', strength: 1 })
+      .sort(sortField)
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .populate([
+        {
+          path: 'variantId',
+          select: 'name'
+        },
+        {
+          path: 'warehouseId',
+          select: 'name'
+        }
+      ])
+      .lean(),
+
+    BatchModel.countDocuments(filter)
+  ])
+
+  const result = {
+    data: batches,
+    meta: {
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit)
+    }
+  }
 
   return result
 }
