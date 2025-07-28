73ba08fc7312b269de65f241ca3550b54c982fad
Huynh Anh <huynhhoanganh1201@gmail.com>
2025-07-28 11:59:22 +0700

    [Deploy] Fix VNPay 2

--- backend/src/services/ordersService.js
+++ backend/src/services/ordersService.js
@@ -162,9 +162,11 @@ const createOrder = async (userId, reqBody, ipAddr, jwtDecoded) => {
 
     // Commit transaction
     await session.commitTransaction()
-
+    console.log('OrderSevice paymentUrl: 0')
     // Xử lý thanh toán VNPAY
     const paymentUrl = orderHelpers.handlePaymentByVnpay(reqBody, order, ipAddr)
+
+    console.log('OrderSevice paymentUrl: ', paymentUrl)
 
     return paymentUrl || order
   } catch (err) {
